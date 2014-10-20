38e7715e426ad71c98e22d63376e6d3b20a5f25e
Paul Gray <pfbgray@gmail.com>
2014-10-20 11:49:25 -0400

    Adding more defaults

--- piechart.js
+++ piechart.js
@@ -5,71 +5,91 @@ angular.module('ngPiechart', [])
         transclude: false,
         scope: false,
         link:function(scope, element, attrs){
-            
+
             var slices = scope.$eval(attrs.model);
             var height = scope.$eval(attrs.height);
             var width  = scope.$eval(attrs.height);
             var config = scope.$eval(attrs.pieChart);
-        
+
             var canvas  = element[0];
             var context = canvas.getContext('2d');
             var width = canvas.width;
             var height = canvas.height;
-            
-            var center = {x:height/2, y:width/2};
-            var radius = width/2;
-            var angle = 0;
+
 
             //variables
             var percentComplete = 0.0;
             var duration = config.duration || 500; //milliseconds
-            
-            var update = function(diff){
-                percentComplete += diff/duration;
-                if (percentComplete >= 1){
-                    percentComplete = 1;
-                    return true;
+            var lineWidth = config.lineWidth || 5; //milliseconds
+
+
+            var center = {x:height/2, y:width/2};
+            var radius = width/2 - lineWidth;
+            var angle = 0;
+
+
+            var startAnimation = function(){
+                slices = scope.$eval(attrs.model);
+                var update = function(diff){
+                    percentComplete += diff/duration;
+                    if (percentComplete >= 1){
+                        percentComplete = 1;
+                        return true;
+                    }
                 }
+
+                var draw = function(context){
+                    context.clearRect(0, 0, width, height);
+                    var angleSum = angle; // 0 for now
+                    for(var i=0; i<slices.length; i++){
+                        context.save();
+                        context.strokeStyle = 'black';
+                        context.lineWidth = config.lineWidth;
+                        context.moveTo(center.x, center.y);
+                        context.beginPath();
+                        var startingAngle = angleSum;
+                        var endingAngle = angleSum += (percentComplete*2*Math.PI) * (slices[i].percentage/100);
+                        context.fillStyle = slices[i].color;
+                        context.arc(center.x, center.y, radius, startingAngle, endingAngle, false);
+                        context.stroke();
+                        context.lineTo(center.x, center.y);
+                        context.fill();
+                        //angleSum += (percentComplete*2*Math.PI) * (slices[i].percentage/100);
+
+                        context.restore();
+                    }
+                };
+
+                var then = new Date();
+
+                var done = false;
+                var interval = setInterval(function(){
+                    var now = new Date();
+                    if(update(now - then)){
+                        clearInterval(interval);
+                        draw(context); //one last time
+                        done = true;
+                    }
+                    if(!done){
+                        draw(context);
+                    }
+                    then = now;
+                }, 0);
             }
-            
-            var draw = function(context){
-                context.save();
-                context.setTransform(1, 0, 0, 1, 0, 0);
-                context.clearRect(0, 0, scope.width, scope.height);
-                context.restore();
-             
-                var angleSum = angle; // 0 for now
-                for(var i=0; i<slices.length; i++){
-                    context.save();
-                    context.moveTo(center.x, center.y);
-                    context.beginPath();
-                    var startingAngle = angleSum;
-                    var endingAngle = angleSum += (percentComplete*2*Math.PI) * (slices[i].percentage/100);
-                    context.fillStyle = slices[i].color;
-                    context.arc(center.x, center.y, radius, startingAngle, endingAngle, false);
-                    context.lineTo(center.x, center.y);
-                    context.fill();
-                    //angleSum += (percentComplete*2*Math.PI) * (slices[i].percentage/100);
-           
-                    context.restore();
+
+            if(slices && slices.length > 0){
+                startAnimation();
+            }
+
+            scope.$watch(attrs.model, function(newVal, oldVal, scope){
+                console.log('change happened on: $scope.' + attrs.model);
+                console.log('   from: ', oldVal);
+                console.log('   to: ', newVal);
+                if(newVal && newVal.length){
+                    startAnimation();
+                    console.log('   kicking off new animation!');
                 }
-            };
-            
-            var then = new Date();
-            
-            var done = false;
-            var interval = setInterval(function(){
-                var now = new Date();
-                if(update(now - then)){
-                    clearInterval(interval);
-                    draw(context); //one last time
-                    done = true;
-                }
-                if(!done){
-                    draw(context);
-                }
-                then = now;
-            }, 32);
+            });
 
         }
     };
